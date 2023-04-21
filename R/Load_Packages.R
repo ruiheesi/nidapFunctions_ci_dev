4fc6dbe0ca3b7830c8b3d654b5c12a181bc7f27c
Maggie Cam <maggie.cam@nih.gov>
2023-04-21 15:28:38 -0400

    First commit

---
+++ R/Load_Packages.R
@@ -0,0 +1,34 @@
+# This code will help load packages with options to turn messages off.
+
+#' @title Load Packages in NIDAP
+#' @description This function will load packages while suppressing startup 
+#'        messages by default.
+#' @details This method provides a way to automatically (optionally) suppress 
+#'        package startup messages in NIDAP
+#'
+#' @param current.package vector of packages that are being used in the template
+#' @param suppress boolean to suppress messages.  By default set to TRUE
+
+#' @export 
+#' 
+#' @return 
+
+
+loadPackages <- function(current.packages,
+                         suppress = TRUE
+                         ){
+    for(package in current.packages){
+      if(suppress == TRUE){
+        suppressPackageStartupMessages(library(package, character.only=TRUE))
+      } else {
+        library(package, character.only=TRUE)
+      }
+    }
+}
+
+
+
+
+
+
+
