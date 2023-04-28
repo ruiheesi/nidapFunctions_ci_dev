772aa85896bed25e70810490714d1a972437a447
Maggie Cam <maggie.cam@nih.gov>
2023-04-28 15:39:35 -0400

    combined library load with reordering

--- R/Load_Packages.R
+++ R/Load_Packages.R
@@ -16,6 +16,29 @@
 loadPackages <- function(current.packages,
                          suppress = TRUE
                          ){
+    
+    basic.packages <- c("package:stats",
+                        "package:graphics",
+                        "package:grDevices",
+                        "package:utils",
+                        "package:datasets",
+                        "package:methods",
+                        "package:base",
+                        "package:SparkR",
+                        "package:vector",
+                        "package:evaluate",
+                        "package:jsonlite",
+                        "package:FoundrySparkR",
+                        "package:futile.logger",
+                        "package:uuid")
+    
+    package.list <- search()[ifelse(unlist(gregexpr("package:",search()))==1,TRUE,FALSE)]
+    package.list <- setdiff(package.list,basic.packages)
+    
+    #If there are existing non-basic packages, detach them.
+    if (length(package.list)>0)  for (package in package.list) detach(package, character.only=TRUE)
+    
+    #load current packages with option to suppress startup messages
     for(package in current.packages){
       if(suppress == TRUE){
         suppressPackageStartupMessages(library(package, character.only=TRUE))
